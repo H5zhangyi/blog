ddd6e5e8d85fe9849ef80e3877d787b9c0841da7
zy1024 <154730619@qq.com>
2018-04-01 14:42:13 +0800

    add params.js

---
+++ 进阶/params.js
@@ -0,0 +1,40 @@
+### Form提交参数转换
+
+```
+// 封装jQuery插件
+$.prototype.serializeObject = function() {
+    var a, o, h, i, e;
+    a = this.serializeArray();
+    o = {};
+    h = o.hasOwnProperty;
+    for (i = 0; i < a.length; i++) {
+        e = a[i];
+        if (!h.call(o, e.name)) {
+            o[e.name] = e.value;
+        }
+    }
+    return o;
+};
+
+// use
+$('form').submit(function(event) {
+    event.preventDefault();
+
+    var form = $(this);
+    $.ajax({
+        url: form.attr("action"),
+        type: "POST",
+        data: JSON.stringify(form.serializeObject()),
+        contentType: "application/json",
+        dataType: "json",
+        beforeSend: function () {
+        },
+        error: function () {
+        },
+        complete:function () {
+        },
+        success: function (res) {
+        }
+    })
+});
+```
